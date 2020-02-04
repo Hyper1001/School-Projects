7b8d629ac2a1d3221227c7b11f534939b4f88a3e
George K Vo <54648764+Hyper1001@users.noreply.github.com>
2020-02-04 07:57:39 -0600

    created vectors, did nothing else.

--- P02/extreme_bonus/main.cpp
+++ P02/extreme_bonus/main.cpp
@@ -1,1 +1,13 @@
+#include "color.h"
+#include <iostream>
+#include <vector>
 
+int main() {
+    Color red{255,0,0};
+    Color green{0,255,0};
+    Color blue{0,0,255};
+    std::vector<Color> colors;
+    colors.push_back(*red);
+    colors.push_back(*green);
+    colors.push_back(*blue);
+}
