a938dd50e7c3ce8c1ed747d6003285d34f719ded
George K Vo <54648764+Hyper1001@users.noreply.github.com>
2020-02-04 07:22:40 -0600

    changed to_string formatting

--- P02/full_credit/color.cpp
+++ P02/full_credit/color.cpp
@@ -21,7 +21,7 @@ Color::Color(int r, int g, int b) : _red{r}, _green {g}, _blue{b}
 
 std::string Color::to_string()
 {
-    return "("+std::to_string(_red)+", "+std::to_string(_green)+", "+std::to_string(_blue)+")";
+    return "("+std::to_string(_red)+","+std::to_string(_green)+","+std::to_string(_blue)+")";
 }
 
 std::string Color::colorize(std::string text)
