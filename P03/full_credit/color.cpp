1dd0c74b1873c085e35174ff430bc520acb726c7
George K Vo <54648764+Hyper1001@users.noreply.github.com>
2020-02-11 05:32:36 -0600

    alsmot done

--- P03/full_credit/color.cpp
+++ P03/full_credit/color.cpp
@@ -2,6 +2,13 @@
 #include "color.h"
 #include <string>
 
+Color::Color()
+{
+    _red = 255;
+    _green = 255;
+    _blue = 255;
+    _reset = true;
+}
 
 Color::Color(int r, int g, int b) : _red{r}, _green {g}, _blue{b}
 {
@@ -17,6 +24,7 @@ Color::Color(int r, int g, int b) : _red{r}, _green {g}, _blue{b}
         _blue = 255;
     else if(b<0)
         _blue=0;
+    _reset = false;
 }
 
 std::string Color::to_string()
@@ -24,7 +32,16 @@ std::string Color::to_string()
     return "("+std::to_string(_red)+","+std::to_string(_green)+","+std::to_string(_blue)+")";
 }
 
-std::string Color::colorize(std::string text)
+std::ostream& operator<<(std::ostream& ost, const Color& color)
 {
-    return "\033[38;2;"+std::to_string(_red)+";"+std::to_string(_green)+";"+std::to_string(_blue)+"m"+text+"\033[0m";
+    if(_reset = true)
+    {
+        ost << "\033[0m\n";
+        return ost;
+    }
+    else
+    {
+        ost <<"\033[38;2;" <<std::to_string(color._red)<<";"<<std::to_string(color._green)<<";"<<std::to_string(color._blue)<<"m";
+        return ost;
+    }
 }
--- P03/full_credit/color.h
+++ P03/full_credit/color.h
@@ -1,16 +1,20 @@
-#ifndef __COLOR_H
+    #ifndef __COLOR_H
 #define __COLOR_H
 #include <string>
 
 class Color
 {
+private:
     int _red;
     int _green;
     int _blue;
+    bool _reset;
 public:
+    Color();
     Color(int r,int g, int b);
     std::string to_string();
-    std::string colorize(std::string text);
+    friend ostream& operator<<(ostream& ost, const Color& color)
+    friend istream& operator>>(istream& ist, Color& color)
 };
 
 #endif
